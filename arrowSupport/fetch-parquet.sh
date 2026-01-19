0d0169586ea370114ae42e4092298760f52f4d7a
Jakub Wlodek <jwlodek.dev@gmail.com>
2026-01-19 15:45:25 -0500

    Add modified gtest sources to build with EPICS build system

--- arrowSupport/fetch-parquet.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget -nv http://archive.apache.org/dist/thrift/0.16.0/thrift-0.16.0.tar.gz
-tar xzf thrift-0.16.0.tar.gz
-cd thrift-0.16.0
-chmod +x ./configure
-./configure --disable-libs
